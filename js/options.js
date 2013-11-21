08592279ca6cdc39fb716d5b626b9369a4b08816
Tom Rudick <tmrudick@gmail.com>
2013-11-20 19:56:44 -0500

    Updating options page

--- js/options.js
+++ js/options.js
@@ -1,37 +1,43 @@
-function getWageType() {
-    var radios = document.getElementsByName('wageType');
-
-    for (var i = 0; i < radios.length; i++) {
-        if (radios[i].checked) {
-            return radios[i].value;
-        }
+function updateOptions(type) {
+    if (type === 'State') {
+        document.getElementById('state').style.display = 'block';
+        document.getElementById('other').style.display = 'none';
+    } else if (type === 'Other') {
+        document.getElementById('state').style.display = 'none';
+        document.getElementById('other').style.display = 'block';
+    } else {
+        document.getElementById('state').style.display = 'none';
+        document.getElementById('other').style.display = 'none';
     }
 }
 
-function setWageType(value) {
-    var radios = document.getElementsByName('wageType');
-
-    for (var i = 0; i < radios.length; i++) {
-        if (radios[i].value === value) {
-            radios[i].checked = true;
-            break;
-        }
-    }
-}
-
+// Load the options when the page loads
 chrome.extension.sendRequest({ name: 'options' }, function(response) {
-    setWageType(response.type || 'federal');
+    // Update the view based on the type
+    updateOptions(response.type);
 
-    document.getElementById('other').value = response.other;
-    document.getElementById('state').value = response.state;
+    // Update all of the fields to have the previously set values
+    document.getElementById('type-dropdown').value = response.type;
+    document.getElementById('other-text').value = response.other;
+    document.getElementById('state-dropdown').value = response.state;
 });
 
+// When the save button is clicked, update the saved values
 document.getElementById('saveBtn').onclick = function() {
     chrome.extension.sendRequest({ name: 'set', options: {
-        type: getWageType(),
-        state: document.getElementById('state').value,
-        other: document.getElementById('other').value
+        type: document.getElementById('type-dropdown').value,
+        state: document.getElementById('state-dropdown').value,
+        other: document.getElementById('other-text').value
     }}, function() {
-        document.getElementById('status').text = 'Saved!';
+        document.getElementById('status').innerHTML = 'Saved!';
     });
 };
+
+// When the type dropdown changes, update the DOM
+document.getElementById('type-dropdown').addEventListener('change', function(event) {
+    updateOptions(event.target.value);
+});
+
+// Prevent flicker
+document.getElementById('state').style.display = 'none';
+document.getElementById('other').style.display = 'none';
